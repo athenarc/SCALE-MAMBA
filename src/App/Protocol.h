38cdc0993ab91fbf550ca88539138d733d7c0c6a
Christos Nasikas <xristosnasikas@gmail.com>
2019-03-12 14:20:24 +0200

    Add handshake state

--- src/App/Protocol.h
+++ src/App/Protocol.h
@@ -2,7 +2,7 @@
 #define _Protocol
 
 namespace sedp {
-  enum class State { INITIAL, RANDOMNESS_SENT, PRIVATE_INPUTS, DATASET_ACCEPTED };
+  enum class State { INITIAL, HANDSHAKE, RANDOMNESS_SENT, PRIVATE_INPUTS, DATASET_ACCEPTED };
 }
 
 #endif
