98eb452eadef0067823b1a3b449c70eaefa99e5e
Christos Nasikas <xristosnasikas@gmail.com>
2019-03-19 20:40:20 +0200

    Create IO_SEDP

--- src/Input_Output/IO_Factory.cpp
+++ src/Input_Output/IO_Factory.cpp
@@ -9,6 +9,9 @@ unique_ptr<IO_Stream> IO_Factory::make_io(unsigned int choice, int player_id)
   else if (choice == IO_FILE) {
     return unique_ptr<IO_Stream>(new Input_Output_File);
   }
+  else if (choice == IO_SEDP) {
+    return unique_ptr<IO_Stream>(new Input_Output_SEDP(player_id));
+  }
 
   return unique_ptr<IO_Stream>(new Input_Output_Simple);
 
