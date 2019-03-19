692415965912c7abe47ddad61feb35c49f1f7495
Christos Nasikas <xristosnasikas@gmail.com>
2019-03-19 14:39:00 +0200

    Simulate SCALE

--- src/Server-Api.cpp
+++ src/Server-Api.cpp
@@ -20,6 +20,32 @@ int main(int argc, const char *argv[]) {
   // use clock as seed
   srand(time(0) + id);
 
+  // open_channel(0)
   Server s(id, 14000 + id, max_clients);
   s.init();
+
+  // open_channel(1)
+  // get_public_input(1)
+  int n = s.get_data_size();
+  cout << "size: " << n << endl;
+
+
+  // open_channel(2)
+  vector<int> shares;
+
+  for (int i = 0; i < n; i++) {
+    shares.push_back(rand() % 1000000);
+  }
+
+  for (int i = 0; i < n; i++) {
+    s.put_random_triple(shares.at(i));
+  }
+
+  // open_channel(3)
+  vector<int>& data = s.get_data();
+
+  for (auto &d : data) {
+    // get_private_input(3);
+    cout << d << endl;
+  }
 } 
